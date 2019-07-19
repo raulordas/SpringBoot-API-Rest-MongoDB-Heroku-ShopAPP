1b65197dfea06d5400fe00ffb60fcbac59f45234
raulordas <ordasraul@gmail.com>
2019-07-19 12:20:51 +0200

    cors

--- src/main/java/com/raul/mongodelivery/security/RestSecurityConfig.java
+++ src/main/java/com/raul/mongodelivery/security/RestSecurityConfig.java
@@ -32,10 +32,8 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
+		http.cors().and().httpBasic()
 		.and().csrf().disable()
-		.authorizeRequests()
-		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().and()
 		.sessionManagement()
 		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
